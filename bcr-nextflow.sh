5af5ddc6ae4384bad60fd8d796c147fbe5a2418b
bts76 <117182944+bts76-pitt@users.noreply.github.com>
2023-10-25 13:35:58 -0400

    Update bcr-nextflow.sh
    sanitized paths

--- bcr-nextflow.sh
+++ bcr-nextflow.sh
@@ -10,6 +10,5 @@
 #SBATCH --mail-user=your_email@sample.com
 
 module load singularity/3.9.6 nextflow/23.04.2 squashfs-tools/4.4 gcc/12.2.0 r/4.3.0
-
-cd /ix/drajasundaram/drajasundaram/shared_bts76_dhr11/BCR_pipeline/bcr-test
-nextflow run ../BCR_nextflow/main.nf -profile slurm -resume -work-dir ./work
+cd BCR_nextflow
+nextflow run ./main.nf -profile slurm -resume -work-dir ./work
